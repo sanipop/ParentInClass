2b5335092d90992ce6af8d9359a0a67672aaa4e1
sanipop <67932273+sanipop@users.noreply.github.com>
2024-08-21 06:44:29 +0100

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,17 @@
+# app/models.py
+from app import db
+from flask_login import UserMixin
+from app import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    role = db.Column(db.String(10), nullable=False, default='parent')  # Default role as 'parent'
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.role}')"
