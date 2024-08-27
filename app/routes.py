21d639381dfcd22c5301eb9fcf5623bff7e5f268
sanipop <67932273+sanipop@users.noreply.github.com>
2024-08-27 07:13:10 +0100

    Admin Route is created:
    The logic for admin is implemented; The page to link all the admin operations

--- app/routes.py
+++ app/routes.py
@@ -15,3 +15,11 @@ main = Blueprint('main', __name__)
 @main.route('/index')
 def index():
     return render_template('index.html')
+
+@main.route('/admin_dashboard')
+@login_required
+def admin_dashboard():
+    if current_user.role != 'admin':
+        flash('Unauthorized access!', 'danger')
+        return redirect(url_for('main.index'))
+    return render_template('admin_dashboard.html')
