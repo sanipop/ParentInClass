554b52926bf65d19da672d5ebab7251a18e3ba0a
sanipop <67932273+sanipop@users.noreply.github.com>
2024-08-27 08:07:33 +0100

    Created parent Registration FlaskForm:
    A flask form is created for form definition and creation for parent registration

---
+++ app/register_forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+class ParentRegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register as Parent')
+    child_name = SelectField('Child Name', choices=[], validators=[DataRequired()])  # Add the choices dynamically
+    submit = SubmitField('Register as Parent')
