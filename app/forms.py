88716ba8e8284f014f91eb1da55c6c4322d27c7d
sanipop <67932273+sanipop@users.noreply.github.com>
2024-08-27 07:59:20 +0100

    Added Form Classes:
    Form classes added to implement the addition of homework, classwork, test. exam and attendance by administrator

--- app/forms.py
+++ app/forms.py
@@ -13,3 +13,32 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+#forms for Admin Function
+class AddStudentForm(FlaskForm):
+    child_name = StringField('Child Name', validators=[DataRequired()])
+    submit = SubmitField('Add Student')
+
+class MarkAttendanceForm(FlaskForm):
+    child_name = SelectField('Child Name', choices=[], validators=[DataRequired()])
+    attendance = SelectField('Attendance', choices=[('Present', 'Present'), ('Absent', 'Absent')], validators=[DataRequired()])
+    submit = SubmitField('Mark Attendance')
+
+class AddTestForm(FlaskForm):
+    child_name = SelectField('Child Name', choices=[], validators=[DataRequired()])
+    test_score = FloatField('Test Score', validators=[DataRequired()])
+    submit = SubmitField('Add Test Score')
+
+class AddExamForm(FlaskForm):
+    child_name = SelectField('Child Name', choices=[], validators=[DataRequired()])
+    exam_score = FloatField('Exam Score', validators=[DataRequired()])
+    submit = SubmitField('Add Exam Score')
+
+class AddClassworkForm(FlaskForm):
+    child_name = SelectField('Child Name', choices=[], validators=[DataRequired()])
+    classwork_score = FloatField('Classwork Score', validators=[DataRequired()])
+    submit = SubmitField('Add Classwork Score')
+
+class AddHomeworkForm(FlaskForm):
+    child_name = SelectField('Child Name', choices=[], validators=[DataRequired()])
+    homework_score = FloatField('Homework Score', validators=[DataRequired()])
+    submit = SubmitField('Add Homework Score')
