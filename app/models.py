180c42a974865d4aec6d2b4880c5c876d7aa7bd6
sanipop <67932273+sanipop@users.noreply.github.com>
2024-08-27 07:49:07 +0100

    Modified User:
    User Class modified to recognize user by role

--- app/models.py
+++ app/models.py
@@ -1,17 +1,14 @@
 # app/models.py
-from app import db
 from flask_login import UserMixin
-from app import login_manager
+from datetime import datetime
+from app import db
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    role = db.Column(db.String(10), nullable=False, default='parent')  # Default role as 'parent'
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    role = db.Column(db.String(10))  # "admin" or "parent"
+    # Define the relationship
+    children = db.relationship('ChildRecord', backref='parent', lazy=True)
     
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.role}')"
