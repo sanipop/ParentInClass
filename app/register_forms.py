be00fc8664a49d6f8448c0e18f99e2ca789645c1
sanipop <67932273+sanipop@users.noreply.github.com>
2024-08-27 08:25:25 +0100

    Added AdiminregistrationForm
    Create a FlaskForm to implement the admin registration

--- app/register_forms.py
+++ app/register_forms.py
@@ -10,3 +10,10 @@ class ParentRegistrationForm(FlaskForm):
     submit = SubmitField('Register as Parent')
     child_name = SelectField('Child Name', choices=[], validators=[DataRequired()])  # Add the choices dynamically
     submit = SubmitField('Register as Parent')
+
+class AdminRegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register as Administrator')
