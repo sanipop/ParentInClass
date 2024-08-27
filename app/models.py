b061005d38c5fcf70930f980030ae8e6c182d643
sanipop <67932273+sanipop@users.noreply.github.com>
2024-08-27 07:51:19 +0100

    Added ChildRecord Class:
    child record class created, with parent id file to link parent Id to Child

--- app/models.py
+++ app/models.py
@@ -12,3 +12,20 @@ class User(db.Model, UserMixin):
     # Define the relationship
     children = db.relationship('ChildRecord', backref='parent', lazy=True)
     
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class ChildRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    child_name = db.Column(db.String(64), index=True)
+    parent_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    attendance = db.Column(db.String(10))  # Example: "Present" or "Absent"
+    classwork_score = db.Column(db.Float)
+    homework_score = db.Column(db.Float)
+    test_score = db.Column(db.Float)
+    exam_score = db.Column(db.Float)
+    notes = db.Column(db.Text)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<ChildRecord {self.child_name}>'
